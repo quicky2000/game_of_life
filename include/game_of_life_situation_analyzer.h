3086a1517b5839d76d6b71e5925beaf54249f1e1
Julien Thevenon <julien_thevenon@yahoo.fr>
2009-01-13 00:46:22 +0100

    Improving indentation

--- include/game_of_life_situation_analyzer.h
+++ include/game_of_life_situation_analyzer.h
@@ -7,13 +7,13 @@
 
 class game_of_life_situation_analyzer: public FSM_situation_analyzer<game_of_life_situation,game_of_life_transition>
 {
-	public:
-		// Constructors and Destructors
-		game_of_life_situation_analyzer(void);
-		~game_of_life_situation_analyzer(void);
-
-		// Methods inherited from FSM_situation_analyzer
-		string toString(void);
-		vector<game_of_life_transition*> getTransitions(game_of_life_situation* p_situation);
+ public:
+  // Constructors and Destructors
+  game_of_life_situation_analyzer(void);
+  ~game_of_life_situation_analyzer(void);
+  
+  // Methods inherited from FSM_situation_analyzer
+  string toString(void);
+  vector<game_of_life_transition*> getTransitions(game_of_life_situation* p_situation);
 };
 #endif /* _GAME_OF_LIFE_SITUATION_ANALYZER_ */
